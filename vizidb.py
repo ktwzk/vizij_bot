6ec637d464d7d8d87ad9a020c74594a90adef651
Vincent Kotwizkiy <vincent@kotwizkiy.ru>
2016-12-20 16:59:02 +0600

    Рефакторнул работу с базой

--- vizidb.py
+++ vizidb.py
@@ -35,7 +35,7 @@ class Link(peewee.Model):
         database = db
 
 
-def db_decorator(action):
+def db_action(action):
     def db_wrapper(*args, **kwargs):
         db.connect()
         result = action(*args, **kwargs)
@@ -44,76 +44,76 @@ def db_decorator(action):
     return db_wrapper
 
 
-@db_decorator
+@db_action
 def create_tables():
     db.create_tables([User, Link])
 
 
-@db_decorator
+@db_action
 def create_user(username):
     user, done = User.create_or_get(username=username)
     user.save()
 
 
-@db_decorator
+@db_action
 def set_name(username, name):
     user = User.get(username=username)
     user.name = name
     user.save()
 
 
-@db_decorator
+@db_action
 def set_bio(username, bio):
     user = User.get(username=username)
     user.bio = bio
     user.save()
 
 
-@db_decorator
+@db_action
 def set_birthday(username, date):
     user = User.get(username=username)
     user.birthday = date
     user.save()
 
 
-@db_decorator
+@db_action
 def set_image(username, image):
     user = User.get(username=username)
     user.image = image
     user.save()
 
 
-@db_decorator
+@db_action
 def set_email(username, email):
     user = User.get(username=username)
     user.email = email
     user.save()
 
 
-@db_decorator
+@db_action
 def set_phone(username, phone):
     user = User.get(username=username)
     user.phone = phone
     user.save()
 
 
-@db_decorator
+@db_action
 def new_link(username, url, description):
     user = User.get(username=username)
     link = Link.create(link=url, description=description, owner=user)
     link.save()
 
 
-@db_decorator
+@db_action
 def del_link(username, description):
     link = Link.select().where(Link.description == description).get()
     link.delete_instance()
 
 
-@db_decorator
+@db_action
 def get_links(username):
     user = User.get(username=username)
     links = {}
     for link in user.links:
-        links[str(link.description)] = str(link.link)
+        links[link.description] = link.link
     return links
